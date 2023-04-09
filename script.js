df2cb2aecb9d85626c8d46206bd6623fa31600b3
unknown <pawanrataiemtieng@gmail.com>
2023-04-09 22:05:54 +0700

    web weARTist

---
+++ script.js
@@ -0,0 +1,10 @@
+/*let text=document.getElementById('text');
+let kon1=document.getElementById('kon1');
+let kon2=document.getElementById('kon2');
+
+window.addEventListener('scroll',() =>{
+    let value = window.scrollY;
+    text.style.marginTop = value*2.5+'px';
+    kon1.style.right = value*-1.5+'px';
+    kon2.style.left = value*-2.5+'px';
+});*/
